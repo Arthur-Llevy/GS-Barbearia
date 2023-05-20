e8f799adc6fa81b38b429f16650fddd7ff70ad9b
Arthur Levy <arthurlbvieiraa@gmail.com>
2023-05-19 23:29:18 -0300

    Adicionado opção de solicitar corte e notificações.

--- src/styles/components/footer.jsx
+++ src/styles/components/footer.jsx
@@ -4,7 +4,7 @@ export const FooterContainer = styled.div`
 	background-color: #171717;
 	height: 60px;
 	bottom: 0;
-	position: fixed;
+	position: absolute;
 	width: 100%;
 	display: flex;
 	justify-content: center;
--- src/styles/components/menu.jsx
+++ src/styles/components/menu.jsx
@@ -6,7 +6,7 @@ export const ContainerMenu = styled.div`
 	height: 65px;
 	display: flex;
 	justify-content: space-around;
-	align-items: center;
+	align-items: center;	
 	
 	img {
 	  width: 10vw;
@@ -20,4 +20,37 @@ export const ContainerMenu = styled.div`
 	img:nth-child(3){
 		transform: scaleX(-1);
 	}
+
+	svg {
+		color: #eee;
+		width: 30px;
+		height: 30px;
+	}
+
+`;
+
+export const PopUp = styled.div`
+	position: absolute;
+	background-color: #212121;
+	width: 90%;
+	border-radius: 10px;
+	height: 80px;
+	display: none;
+	flex-direction: column;
+	gap: 10px;
+	justify-content: center;
+	align-items: center;
+	top: 70px;
+	left: 50%;
+	transform: translateX(-50%);
+	opacity: 0;
+	display: none;
+	transition: .2s;
+
+	p {
+	  color: #eee;
+	  text-decoration: underline;
+	  cursor: pointer;
+	}
+
 `;
